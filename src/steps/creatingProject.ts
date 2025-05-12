35a6d24f64aaa9ff216e6cacdcd0dc5ed81a90e7
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-05-12 22:44:09 +0200

    feat: refactor project creation to use tasks for improved structure and async handling

--- src/steps/creatingProject.ts
+++ src/steps/creatingProject.ts
@@ -1,4 +1,4 @@
-import { cancel, confirm, isCancel, log } from "@clack/prompts";
+import { cancel, confirm, isCancel, tasks } from "@clack/prompts";
 import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
@@ -26,102 +26,107 @@ export default async function creatingProject({
     projectName: string;
     identifier: string;
 }) {
-    log.info(`Creating project in ${rootFolder}...`);
-    switch (overwrite) {
-        case OverwriteEnum.Delete:
-            emptyDir(rootFolder);
-            break;
-        case OverwriteEnum.Overwrite:
-        case OverwriteEnum.Skip:
-        case OverwriteEnum.Ask:
-            break;
-        default:
-            fs.mkdirSync(rootFolder, { recursive: true });
-            break;
-    }
+    await tasks([
+        {
+            title: `Creating project in ${rootFolder}...`,
+            task: async (message) => {
+                switch (overwrite) {
+                    case OverwriteEnum.Delete:
+                        emptyDir(rootFolder);
+                        break;
+                    case OverwriteEnum.Overwrite:
+                    case OverwriteEnum.Skip:
+                    case OverwriteEnum.Ask:
+                        break;
+                    default:
+                        fs.mkdirSync(rootFolder, { recursive: true });
+                        break;
+                }
 
-    const templateDir = path.resolve(fileURLToPath(import.meta.url), "../..", `${template}`);
+                const templateDir = path.resolve(fileURLToPath(import.meta.url), "../..", `${template}`);
 
-    const write = (file: string, content?: string) => {
-        const targetPath = path.join(rootFolder, renameFiles[file] ?? file);
-        if (content) {
-            fs.writeFileSync(targetPath, content);
-        } else {
-            copy(path.join(templateDir, file), targetPath);
-        }
-    };
-
-    const filesNames = fs.readdirSync(templateDir);
-    for (const fileName of filesNames) {
-        const filePath = path.join(rootFolder, renameFiles[fileName] ?? fileName);
-        if (fs.existsSync(filePath)) {
-            switch (overwrite) {
-                case OverwriteEnum.Ask:
-                    const overwrite = await confirm({
-                        message: `File ${filePath} already exists. Do you want to overwrite it?`,
-                        initialValue: false,
-                    });
-                    if (isCancel(overwrite)) {
-                        cancel("Operation cancelled.");
-                        process.exit(0);
+                const write = (file: string, content?: string) => {
+                    const targetPath = path.join(rootFolder, renameFiles[file] ?? file);
+                    if (content) {
+                        fs.writeFileSync(targetPath, content);
+                    } else {
+                        copy(path.join(templateDir, file), targetPath);
                     }
-                    if (!overwrite) {
-                        continue;
+                };
+
+                const filesNames = fs.readdirSync(templateDir);
+                for (const fileName of filesNames) {
+                    const filePath = path.join(rootFolder, renameFiles[fileName] ?? fileName);
+                    if (fs.existsSync(filePath)) {
+                        switch (overwrite) {
+                            case OverwriteEnum.Ask:
+                                const overwrite = await confirm({
+                                    message: `File ${filePath} already exists. Do you want to overwrite it?`,
+                                    initialValue: false,
+                                });
+                                if (isCancel(overwrite)) {
+                                    cancel("Operation cancelled.");
+                                    process.exit(0);
+                                }
+                                if (!overwrite) {
+                                    continue;
+                                }
+                                fs.rmSync(filePath, { recursive: true, force: true });
+                                break;
+                            case OverwriteEnum.Skip:
+                                continue;
+                            case OverwriteEnum.Delete:
+                            case OverwriteEnum.Overwrite:
+                                fs.rmSync(filePath, { recursive: true, force: true });
+                                break;
+                        }
                     }
-                    fs.rmSync(filePath, { recursive: true, force: true });
-                    break;
-                case OverwriteEnum.Skip:
-                    continue;
-                case OverwriteEnum.Delete:
-                case OverwriteEnum.Overwrite:
-                    fs.rmSync(filePath, { recursive: true, force: true });
-                    break;
-            }
-        }
-        switch (fileName) {
-            case "package.json":
-            case "vite.config.ts":
-            case "index.html":
-                let file = fs.readFileSync(path.join(templateDir, fileName), "utf-8");
-                file = file.replace(/my-app-package-name/g, packageName);
-                file = file.replace(/my-app-description/g, description);
-                file = file.replace(/my-app-project-name/g, projectName);
-                write(fileName, file);
-                break;
-            case ".git":
-            case "package-lock.json":
-                break;
-            default:
-                write(fileName);
-        }
-    }
+                    switch (fileName) {
+                        case "package.json":
+                        case "vite.config.ts":
+                        case "index.html":
+                            let file = fs.readFileSync(path.join(templateDir, fileName), "utf-8");
+                            file = file.replace(/my-app-package-name/g, packageName);
+                            file = file.replace(/my-app-description/g, description);
+                            file = file.replace(/my-app-project-name/g, projectName);
+                            write(fileName, file);
+                            break;
+                        case ".git":
+                        case "package-lock.json":
+                            break;
+                        default:
+                            write(fileName);
+                    }
+                }
 
-    // if exist root/src-tauri folder, copy it to root folder
-    const srcTauriDir = path.join(rootFolder, "src-tauri");
-    if (fs.existsSync(srcTauriDir)) {
-        const filesNames = fs.readdirSync(srcTauriDir);
-        for (const fileName of filesNames) {
-            switch (fileName) {
-                case "Cargo.lock":
-                case "Cargo.toml":
-                case "tauri.conf.json":
-                    let file = fs.readFileSync(path.join(srcTauriDir, fileName), "utf-8");
-                    file = file.replace(/my-app-package-name/g, packageName);
-                    file = file.replace(/my-app-description/g, description);
-                    file = file.replace(/my-app-project-name/g, projectName);
-                    file = file.replace(/com.my-app-project-name.app/g, identifier);
-                    write(path.join("src-tauri", fileName), file);
-            }
-        }
-    }
+                // if exist root/src-tauri folder, copy it to root folder
+                const srcTauriDir = path.join(rootFolder, "src-tauri");
+                if (fs.existsSync(srcTauriDir)) {
+                    const filesNames = fs.readdirSync(srcTauriDir);
+                    for (const fileName of filesNames) {
+                        switch (fileName) {
+                            case "Cargo.lock":
+                            case "Cargo.toml":
+                            case "tauri.conf.json":
+                                let file = fs.readFileSync(path.join(srcTauriDir, fileName), "utf-8");
+                                file = file.replace(/my-app-package-name/g, packageName);
+                                file = file.replace(/my-app-description/g, description);
+                                file = file.replace(/my-app-project-name/g, projectName);
+                                file = file.replace(/com.my-app-project-name.app/g, identifier);
+                                write(path.join("src-tauri", fileName), file);
+                        }
+                    }
+                }
 
-    // if exist root/_github folder, rename it to .github
-    const srcGitHubDir = path.join(rootFolder, "_github");
-    if (fs.existsSync(srcGitHubDir)) {
-        fs.renameSync(srcGitHubDir, path.join(rootFolder, ".github"));
-    }
-
-    log.success(`Done.`);
+                // if exist root/_github folder, rename it to .github
+                const srcGitHubDir = path.join(rootFolder, "_github");
+                if (fs.existsSync(srcGitHubDir)) {
+                    fs.renameSync(srcGitHubDir, path.join(rootFolder, ".github"));
+                }
+                return "Creating project done";
+            },
+        },
+    ]);
 }
 
 function copyDir(srcDir: string, destDir: string) {
